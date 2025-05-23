8e8f0d9a865d81ab89ec1b4fd3255a75a3cb13a0
GUYEIEI <guy18042003@gmail.com>
2025-05-23 15:12:36 +0700

    refactor: progress bar component

--- src/component/dashboard/ProgressBar.tsx
+++ src/component/dashboard/ProgressBar.tsx
@@ -2,19 +2,23 @@ import React from 'react'
 
 
 type ProgressBarProps = {
-    progress: number; //  0 - 100
+    progress: number;         // ค่าเปอร์เซ็นต์ 0-100
+    width?: string;           // ความกว้าง เช่น '100%', '200px'
+    height?: string;          // ความสูง เช่น '4px', '8px'
+    color?: string;           // สีของแถบ (ถ้าต้องการ)
+    bgColor?: string;         // สี background ของแถบเบื้องหลัง
 };
 
-const ProgressBar = ({ progress }: ProgressBarProps) => {
+const ProgressBar = ({ progress, width = "100%", height = "16px", color = "bg-orange-500", bgColor="bg-gray-100" }: ProgressBarProps) => {
     
     return (
-        <div className="w-full bg-gray-100 rounded-full h-[12px] overflow-hidden mb-[3px]">
-            <div
-                className="bg-orange-600 h-[12px] rounded-full transition-all duration-300 ease-in-out"
-                style={{ width: `${progress}%` }}
-            />
+        <div className={`${bgColor} rounded-full overflow-hidden`} style={{ width, height }}>
+        <div
+            className={`${color} h-full rounded-full transition-all duration-300 ease-in-out`}
+            style={{ width: `${progress}%` }}
+        />
         </div>
-    );
+  );
 }
 
 export default ProgressBar
