5ebe84e7a579a72d05b0cf2a7c091e36dc286af0
GUYEIEI <guy18042003@gmail.com>
2025-05-23 15:12:02 +0700

    refactor: finish assignment component

--- src/component/dashboard/finishAssignment.tsx
+++ src/component/dashboard/finishAssignment.tsx
@@ -12,13 +12,13 @@ const FinishAssignment = () => {
           <div className="w-10 h-10 flex items-center justify-center border border-gray-200 rounded-xl shadow-sm m-2">
             <MdOutlineAssignmentTurnedIn className="text-gray-300 text-2xl"/>
           </div>
-          <div className='flex items-end m-2 '>
-            <div>
+          <div className='flex items-end m-2'>
+            <div className="w-full mr-1 mb-[4px]">
               <h1 className='text-md font-bold pb-2 '>To do Assignment done</h1>
-              <ProgressBar progress={20} />
+              <ProgressBar progress={74} height="12px"/>
             </div>
             <div className="flex ml-auto">
-              <p className="text-orange-600 text-sm">14/30</p> 
+              <p className="text-orange-500 text-sm">14/30</p> 
               <span className="text-sm text-gray-500 ml-1">class</span> 
             </div>
           </div>
