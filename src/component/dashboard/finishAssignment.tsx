9f0b4944a24290054e99e3f28623e0d1ef38f267
GUYEIEI <guy18042003@gmail.com>
2025-05-26 03:59:00 +0700

    refactor: tailwind css

--- src/component/dashboard/finishAssignment.tsx
+++ src/component/dashboard/finishAssignment.tsx
@@ -8,11 +8,11 @@ const FinishAssignment = () => {
     <div className='h-full'>
       <h1 className='text-xl mx-2'>Finish Assignments</h1>
       <div>
-        <div className='border border-gray-200 rounded-lg my-5 mx-2 p-2'>
+        <div className='border border-gray-200 rounded-lg md:my-7  p-2'>
           <div className="w-10 h-10 flex items-center justify-center border border-gray-200 rounded-xl shadow-sm m-2">
             <MdOutlineAssignmentTurnedIn className="text-gray-300 text-2xl"/>
           </div>
-          <div className='flex items-end m-2'>
+          <div className='flex items-end m-2 '>
             <div className="w-full mr-1 mb-[4px]">
               <h1 className='text-md font-bold pb-2 '>To do Assignment done</h1>
               <ProgressBar progress={74} height="12px"/>
