cb84b2358a0f0f03117c0aa0866334203931b7f9
GUYEIEI <guy18042003@gmail.com>
2025-05-26 04:01:27 +0700

    refactor: add map data function

--- src/component/dashboard/Resource.tsx
+++ src/component/dashboard/Resource.tsx
@@ -1,85 +1,88 @@
 import React from 'react'
-import { BiSolidFilePdf } from "react-icons/bi";
+import { BiSolidFilePdf, BiSolidFilePng, BiSolidFileJpg } from "react-icons/bi";
 import ProgressBar from './ProgressBar';
-import { BiSolidFilePng } from "react-icons/bi";
-import { BiSolidFileJpg } from "react-icons/bi";
+
+const mockResources = [
+  {
+    id: 1,
+    type: 'pdf',
+    icon: BiSolidFilePdf,
+    title: 'Node For Beginner',
+    size: '4.5mb',
+    progress: 90,
+    progressColor: 'bg-orange-500',
+    progressBgColor: 'bg-orange-100',
+    iconColor: '#F97316' // Tailwind orange-500 hex code
+  },
+  {
+    id: 2,
+    type: 'jpg',
+    icon: BiSolidFileJpg,
+    title: 'React Intro',
+    size: '3.2mb',
+    progress: 50,
+    progressColor: 'bg-blue-500',
+    progressBgColor: 'bg-blue-100',
+    iconColor: '#3B82F6' // Tailwind blue-500 hex code
+  },
+  {
+    id: 3,
+    type: 'png',
+    icon: BiSolidFilePng,
+    title: 'CSS Basics',
+    size: '2.8mb',
+    progress: 80,
+    progressColor: 'bg-green-500',
+    progressBgColor: 'bg-green-100',
+    iconColor: '#22C55E' // Tailwind green-500 hex code
+  }
+];
 
 const Resource = () => {
   return (
     <div className='h-full mx-2'>
-      <h1 className='text-xl ml-2 '>Yours Resources</h1>
+      <div className='flex justify-between'>
+        <h1 className='text-xl ml-2 '>Yours Resources</h1>
+        <button className='text-orange-600 text-sm p-1 px-2 cursor-pointer bg-orange-100 rounded-lg font-bold'>see more</button>
+      </div>
       <div className='w-full mt-3'>
-
-        <div className='flex my-3 '>
-          {/* icon */}
-          <div className='w-10 h-10'>
-            <BiSolidFilePdf className='text-[2.50rem] text-red-600'/>
-          </div>
-          {/* detail */}
-          <div className='w-full'>
-            <div className='h-full flex flex-col mx-3'>
-              <div  className='h-1/2 flex items-center justify-between'>
-                <h1 className='text-sm font-bold '>Node For Brginner</h1>
-                <span className='text-sm'>4.5mb</span>
+        {mockResources.map(resource => {
+          const IconComponent = resource.icon;
+          return (
+            <div key={resource.id} className='flex my-3'>
+              {/* icon */}
+              <div className='w-10 h-10'>
+                <IconComponent 
+                className='text-[2.50rem]' 
+                style={{ color: resource.iconColor }} 
+              />
               </div>
-              {/* <span className='text-sm text-gray-400'>example for everything in node js / ts</span> */}
-              <div className='h-1/2 mt-2'>
-                <ProgressBar progress={90} width="100%" height="5px" color="bg-orange-500" bgColor="bg-orange-100"/>
+              {/* detail */}
+              <div className='w-full'>
+                <div className='h-full flex flex-col mx-3'>
+                  <div className='h-1/2 flex items-center justify-between'>
+                    <h1 className='text-sm font-bold '>{resource.title}</h1>
+                    <span className='text-sm'>{resource.size}</span>
+                  </div>
+                  <div className='h-1/2 mt-2'>
+                    <ProgressBar 
+                      progress={resource.progress} 
+                      width="100%" 
+                      height="5px" 
+                      color={resource.progressColor} 
+                      bgColor={resource.progressBgColor} 
+                    />
+                  </div>
+                </div>
               </div>
+              {/* button */}
+              <button className='text-orange-600 text-sm p-2 cursor-pointer'>Download</button>
             </div>
-          </div>
-          {/* button */}
-          <button className='text-orange-600 text-sm p-2 cursor-pointer'>Download</button>
-        </div>
-
-        <div className='flex  my-3'>
-          {/* icon */}
-          <div className='w-10 h-10'>
-            <BiSolidFileJpg className='text-[2.50rem] text-blue-600'/>
-            {/* <BiSolidFilePdf className='text-[2.50rem] text-red-600'/> */}
-          </div>
-          {/* detail */}
-          <div className='w-full'>
-            <div className='h-full flex flex-col mx-3'>
-              <div  className='h-1/2 flex items-center justify-between'>
-                <h1 className='text-sm font-bold '>Node For Brginner</h1>
-                <span className='text-sm'>4.5mb</span>
-              </div>
-              {/* <span className='text-sm text-gray-400'>example for everything in node js / ts</span> */}
-              <div className='h-1/2 mt-2'>
-                <ProgressBar progress={50} width="100%" height="5px" color="bg-orange-500" bgColor="bg-orange-100"/>
-              </div>
-            </div>
-          </div>
-          {/* button */}
-          <button className='text-orange-600 text-sm p-2 cursor-pointer'>Download</button>
-        </div>
-
-        <div className='flex  my-3'>
-          {/* icon */}
-          <div className='w-10 h-10'>
-            <BiSolidFilePng className='text-[2.50rem] text-green-600'/>
-            {/* <BiSolidFilePdf className='text-[2.50rem] text-red-600'/> */}
-          </div>
-          {/* detail */}
-          <div className='w-full'>
-            <div className='h-full flex flex-col mx-3'>
-              <div  className='h-1/2 flex items-center justify-between'>
-                <h1 className='text-sm font-bold '>Node For Beginner</h1>
-                <span className='text-sm'>4.5mb</span>
-              </div>
-              {/* <span className='text-sm text-gray-400'>example for everything in node js / ts</span> */}
-              <div className='h-1/2 mt-2'>
-                <ProgressBar progress={80} width="100%" height="5px" color="bg-orange-500" bgColor="bg-orange-100"/>
-              </div>
-            </div>
-          </div>
-          {/* button */}
-          <button className='text-orange-600 text-sm p-2 cursor-pointer'>Download</button>
-        </div>
+          )
+        })}
       </div>
     </div>
   )
 }
 
-export default Resource
+export default Resource;
