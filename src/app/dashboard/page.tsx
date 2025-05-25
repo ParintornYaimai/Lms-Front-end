48982dfc50d75848f4821c81276c4e01c9e961be
GUYEIEI <guy18042003@gmail.com>
2025-05-26 04:05:44 +0700

    feat: add animation in dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import FinishAssignment from '@/component/dashboard/FinishAssignment'
 import Resource from '@/component/dashboard/Resource'
+import MiniCalendar from '@/component/dashboard/MiniCalendar'
+import BarChartComponent from '@/component/dashboard/BarChart'
+import TaskProgress from '@/component/dashboard/TaskProgress'
+import TodoList from '@/component/dashboard/Todolist'
+
 
 const Dashboard = () => {
   return (
@@ -16,35 +21,43 @@ const Dashboard = () => {
         <div className='mt-10 '>
           <div className='flex gap-8'>
             {/* one */}
-            <div className="basis-1/4 max-h-[220px] rounded-lg border border-gray-200 shadow-lg p-4">
+            <div className="basis-1/4 max-h-[220px] rounded-lg border border-gray-200 shadow-lg p-4 hover:scale-101 transition-transform duration-300">
               <FinishAssignment/>
             </div>
             {/* two */}
-            <div className="basis-2/4 h-[220px] rounded-lg border border-gray-200 shadow-lg p-4 overflow-auto">
+            <div className="basis-2/4 h-[220px] rounded-lg border border-gray-200 shadow-lg p-4 overflow-auto hover:scale-101 transition-transform duration-300">
               <Resource/>
             </div>
             {/* three */}
-            <div className="basis-1/4 max-h-[220px] rounded-lg border border-gray-200 shadow-lg p-4">Box 3</div>
+            <div className="basis-1/4 max-h-[220px] rounded-lg border border-gray-200 shadow-lg hover:scale-101 transition-transform duration-300">
+              <MiniCalendar/>
+            </div>
           </div>
         </div>
 
         <div className='mt-10'>
           <div className='flex gap-8'>
             {/* one */}
-            <div className="flex-1 max-h-[300px] rounded-lg border border-gray-200 shadow-lg p-4">Box 1</div>
+            <div className="flex-1 max-h-[300px] rounded-lg border border-gray-200 shadow-lg p-4 hover:scale-101 transition-transform duration-300">
+              <BarChartComponent/>
+            </div>
             {/* two */}
-            <div className="flex-1 h-[300px] rounded-lg border border-gray-200 shadow-lg p-4 overflow-auto">Box 2</div>
+            <div className="flex-1 h-[300px] rounded-lg border border-gray-200 shadow-lg p-4 overflow-auto hover:scale-101 transition-transform duration-300">
+              <TaskProgress/>
+            </div>
             {/* three */}
-            <div className="flex-1 max-h-[300px] rounded-lg border border-gray-200 shadow-lg p-4">Box 3</div>
+            <div className="flex-1 max-h-[300px] rounded-lg border border-gray-200 shadow-lg p-4 hover:scale-101 transition-transform duration-300">
+              <TodoList/>
+            </div>
           </div>
         </div>
 
         <div className='my-10 mx-10'>
           <div className='flex gap-8'>
             {/* one */}
-            <div className="basis-full md:basis-2/4 h-[300px] rounded-lg border border-gray-200 shadow-lg p-4">Box 1</div>
+            <div className="basis-full md:basis-2/4 h-[300px] rounded-lg border border-gray-200 shadow-lg p-4 hover:scale-101 transition-transform duration-300">Box 1</div>
             {/* two */}
-            <div className="basis-full md:basis-2/4 h-[300px] rounded-lg border border-gray-200 shadow-lg p-4 overflow-auto">Box 2</div>
+            <div className="basis-full md:basis-2/4 h-[300px] rounded-lg border border-gray-200 shadow-lg p-4 overflow-auto hover:scale-101 transition-transform duration-300">Box 2</div>
             {/* three */}
           </div>
         </div>
